9f08a40ba238034904c3f10a4828e4d4914322a2
MoisesASantos <moiseshitalukuasantos@gmail.com>
2026-07-06 17:24:31 +0100

    add the logic to upload the image and save into database his url

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"io"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
+	
 	videoIDString := r.PathValue("videoID")
 	videoID, err := uuid.Parse(videoIDString)
 	if err != nil {
@@ -32,6 +33,47 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, _, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := r.Header.Get("Content-Type")
+	if contentType == "" {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+
+	}
+
+	params := thumbnail{
+		data:	data,
+		mediaType: contentType,
+	}
+	
+	videoThumbnails[video.ID] = params
+	url := fmt.Sprintf("http://localhost:8091/api/thumbnails/%v", video.ID,)
+	video.ThumbnailURL = &url
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to update the video", err)
+		return
+	}
+	fmt.Println(video)
+
+	respondWithJSON(w, http.StatusOK, video)
 }
